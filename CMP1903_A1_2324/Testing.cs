e91d1a263bf416093a32bef1faae30da94c033fc
Nathan Crane <84578406+SaltiestLemon@users.noreply.github.com>
2024-03-07 12:22:41 +0000

    Finalised changes and edited logic errors

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CMP1903_A1_2324
 {
@@ -15,23 +16,27 @@ namespace CMP1903_A1_2324
          * Create a Die object and call its method.
          * Use debug.assert() to make the comparisons and tests.
          */
-
+        
         //Method
         public bool Test()
         {
             // Runs a test version of the game, and checks the outputs of the rolls
             Game testGame = new Game();
             testGame.ExecuteGame();
+            int valueRoll1 = testGame.ValueRoll1();
+            int valueRoll2 = testGame.ValueRoll2();
+            int valueRoll3 = testGame.ValueRoll3();
+            var totalOfRolls = testGame.TotalOfRolls();
             Debug.Assert(testGame.TotalOfRolls() < 18);
-            var testTotalOfRolls = testGame.ValueRoll1 + testGame.ValueRoll2 + testGame.ValueRoll3;
+            var testTotalOfRolls = valueRoll1 + valueRoll2 + valueRoll3;
             if (testGame.TotalOfRolls() == testTotalOfRolls)
             {
-                    Console.WriteLine("Total Is As Expected: " + testGame.TotalOfRolls());
+                    Console.WriteLine("Total Is As Expected: " + totalOfRolls.ToString());
                     return true;
             }
             else
             {
-                Console.WriteLine("Total Is Noty As Expected, Error With Addition: " + testGame.TotalOfRolls() + "" + testTotalOfRolls);
+                Console.WriteLine("Total Is Noty As Expected, Error With Addition");
                 return false;
             }
         }
